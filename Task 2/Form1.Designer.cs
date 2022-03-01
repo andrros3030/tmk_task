ec970007875aef853b10718799fe4fa5f6a9d8e0
andrros3030 <66036268+andrros3030@users.noreply.github.com>
2022-03-01 11:44:02 +0300

    major

--- Task 2/Form1.Designer.cs
+++ Task 2/Form1.Designer.cs
@@ -1,6 +1,19 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+public class DoubleBufferedTableLayoutPanel : TableLayoutPanel
+{
+    public DoubleBufferedTableLayoutPanel()
+    {
+        DoubleBuffered = true;
+    }
+}
 namespace Task_2
 {
+
     partial class Form1
     {
         /// <summary>
@@ -29,13 +42,228 @@ namespace Task_2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.create_new_button = new System.Windows.Forms.Button();
+            this.label_note = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.is_good = new System.Windows.Forms.CheckBox();
+            this.steel_selector = new System.Windows.Forms.DomainUpDown();
+            this.number_field = new System.Windows.Forms.TextBox();
+            this.weight_field = new System.Windows.Forms.TextBox();
+            this.size_X = new System.Windows.Forms.TextBox();
+            this.size_Y = new System.Windows.Forms.TextBox();
+            this.size_Z = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.table = new DoubleBufferedTableLayoutPanel();
+            this.cancel_button = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // create_new_button
+            // 
+            this.create_new_button.Location = new System.Drawing.Point(721, 228);
+            this.create_new_button.Name = "create_new_button";
+            this.create_new_button.Size = new System.Drawing.Size(236, 75);
+            this.create_new_button.TabIndex = 0;
+            this.create_new_button.Text = "Добавить данные";
+            this.create_new_button.UseVisualStyleBackColor = true;
+            this.create_new_button.Click += new System.EventHandler(this.create_new_button_Click);
+            // 
+            // label_note
+            // 
+            this.label_note.AutoSize = true;
+            this.label_note.Location = new System.Drawing.Point(720, 10);
+            this.label_note.Name = "label_note";
+            this.label_note.Size = new System.Drawing.Size(62, 17);
+            this.label_note.TabIndex = 3;
+            this.label_note.Text = "№трубы";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(875, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "качество";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(720, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "марка стали";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(720, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "размеры";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(875, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "вес";
+            // 
+            // is_good
+            // 
+            this.is_good.AutoSize = true;
+            this.is_good.Checked = true;
+            this.is_good.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.is_good.Location = new System.Drawing.Point(878, 190);
+            this.is_good.Name = "is_good";
+            this.is_good.Size = new System.Drawing.Size(75, 21);
+            this.is_good.TabIndex = 10;
+            this.is_good.Text = "годная";
+            this.is_good.UseVisualStyleBackColor = true;
+            // 
+            // steel_selector
+            // 
+            this.steel_selector.Location = new System.Drawing.Point(721, 75);
+            this.steel_selector.Name = "steel_selector";
+            this.steel_selector.ReadOnly = true;
+            this.steel_selector.Size = new System.Drawing.Size(150, 22);
+            this.steel_selector.TabIndex = 13;
+            // 
+            // number_field
+            // 
+            this.number_field.Location = new System.Drawing.Point(721, 30);
+            this.number_field.Name = "number_field";
+            this.number_field.Size = new System.Drawing.Size(125, 22);
+            this.number_field.TabIndex = 14;
+            // 
+            // weight_field
+            // 
+            this.weight_field.Location = new System.Drawing.Point(878, 120);
+            this.weight_field.Name = "weight_field";
+            this.weight_field.Size = new System.Drawing.Size(79, 22);
+            this.weight_field.TabIndex = 15;
+            // 
+            // size_X
+            // 
+            this.size_X.Location = new System.Drawing.Point(723, 120);
+            this.size_X.Name = "size_X";
+            this.size_X.Size = new System.Drawing.Size(106, 22);
+            this.size_X.TabIndex = 16;
+            // 
+            // size_Y
+            // 
+            this.size_Y.Location = new System.Drawing.Point(723, 155);
+            this.size_Y.Name = "size_Y";
+            this.size_Y.Size = new System.Drawing.Size(106, 22);
+            this.size_Y.TabIndex = 17;
+            // 
+            // size_Z
+            // 
+            this.size_Z.Location = new System.Drawing.Point(723, 190);
+            this.size_Z.Name = "size_Z";
+            this.size_Z.Size = new System.Drawing.Size(106, 22);
+            this.size_Z.TabIndex = 18;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.table);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(703, 701);
+            this.panel1.TabIndex = 19;
+            // 
+            // table
+            // 
+            this.table.AutoSize = true;
+            this.table.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.table.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.table.ColumnCount = 7;
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.table.Location = new System.Drawing.Point(0, 0);
+            this.table.Name = "table";
+            this.table.RowCount = 1;
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.table.Size = new System.Drawing.Size(8, 2);
+            this.table.TabIndex = 2;
+            // 
+            // cancel_button
+            // 
+            this.cancel_button.Enabled = false;
+            this.cancel_button.Location = new System.Drawing.Point(882, 309);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(75, 23);
+            this.cancel_button.TabIndex = 20;
+            this.cancel_button.Text = "Отмена";
+            this.cancel_button.UseVisualStyleBackColor = true;
+            this.cancel_button.Click += new System.EventHandler(this.cancel_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(969, 725);
+            this.Controls.Add(this.cancel_button);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.size_Z);
+            this.Controls.Add(this.size_Y);
+            this.Controls.Add(this.size_X);
+            this.Controls.Add(this.weight_field);
+            this.Controls.Add(this.number_field);
+            this.Controls.Add(this.steel_selector);
+            this.Controls.Add(this.is_good);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_note);
+            this.Controls.Add(this.create_new_button);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Программа ввода данных по трубам";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button create_new_button;
+        private DoubleBufferedTableLayoutPanel table;
+        private Label label_note;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private CheckBox is_good;
+        private DomainUpDown steel_selector;
+        private TextBox number_field;
+        private TextBox weight_field;
+        private TextBox size_X;
+        private TextBox size_Y;
+        private TextBox size_Z;
+        private Panel panel1;
+        private Button cancel_button;
     }
 }
 
--- Task 2/Form1.cs
+++ Task 2/Form1.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Task_2
@@ -16,5 +12,288 @@ namespace Task_2
         {
             InitializeComponent();
         }
+        List<Tube> session_list = new List<Tube>();
+        List<int> actual_indexes = new List<int>();
+        int editable_index = -1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Dimensions classic_dimentions = new Dimensions(1000.0, 20.0, 20.0);
+            session_list.Add(new Tube(0, classic_dimentions, 5.0, steel_type.steel1));
+            session_list.Add(new Tube(1, classic_dimentions, 5.0, steel_type.steel2));
+            session_list.Add(new Tube(2, classic_dimentions, 5.0, steel_type.steel3, false));
+            Fill_table();
+            steel_selector.Items.Clear();
+            steel_selector.Items.Add("steel1");
+            steel_selector.Items.Add("steel2");
+            steel_selector.Items.Add("steel3");
+            steel_selector.SelectedIndex = 0;
+        }
+        private void Fill_table()
+        {
+            table.Controls.Clear();
+            actual_indexes.Clear();
+            if (session_list.Count > 0)
+            {
+                foreach (KeyValuePair<string, dynamic> el in session_list[0].get_data())
+                {
+                    Label tmp = new Label();
+                    tmp.Text = el.Key.ToString();
+                    tmp.AutoSize = true;
+                    table.Controls.Add(tmp);
+                }
+                Label empty_field = new Label();
+                empty_field.AutoSize = true;
+                table.Controls.Add(empty_field);
+                Label empy_field_2 = new Label();
+                empy_field_2.AutoSize = true;
+                table.Controls.Add(empy_field_2);
+            }
+            for (int i = 0; i < session_list.Count; ++i)
+            {
+                actual_indexes.Add(i);
+                foreach (KeyValuePair<string, dynamic> el in session_list[i].get_data())
+                {
+                    Label tmp = new Label();
+                    tmp.Text = el.Value.ToString();
+                    tmp.AutoSize = true;
+                    table.Controls.Add(tmp);
+                }
+                Button remove = new Button();
+                remove.Text = "Удалить";
+                remove.Name = i.ToString();
+                remove.Click += new EventHandler(remove_item);
+                remove.AutoSize = true;
+                table.Controls.Add(remove);
+                Button edit = new Button();
+                edit.Text = "Изменить";
+                edit.Name = i.ToString();
+                edit.Click += new EventHandler(edit_item);
+                edit.AutoSize = true;
+                table.Controls.Add(edit);
+            }
+        }
+        private void Add_tube(Tube new_tube, int index)
+        {
+            actual_indexes.Add(index);
+            foreach (KeyValuePair<string, dynamic> el in new_tube.get_data())
+            {
+                Label tmp = new Label();
+                tmp.Text = el.Value.ToString();
+                tmp.AutoSize = true;
+                table.Controls.Add(tmp);
+            }
+            Button remove = new Button();
+            remove.Text = "Удалить";
+            remove.Name = index.ToString();
+            remove.Click += new EventHandler(remove_item);
+            remove.AutoSize = true;
+            table.Controls.Add(remove);
+            Button edit = new Button();
+            edit.Text = "Изменить";
+            edit.Name = index.ToString();
+            edit.Click += new EventHandler(edit_item);
+            edit.AutoSize = true;
+            table.Controls.Add(edit);
+        }
+        private void Update_tube(int index)
+        {
+            int f_index = actual_indexes.Where((it, i2) => { return it == index; }).First();
+            int s_index = actual_indexes.IndexOf(f_index);
+            System.Diagnostics.Debug.WriteLine(f_index);
+            System.Diagnostics.Debug.WriteLine(s_index);
+            for (int _ = 0; _ < table.ColumnCount; _++)
+                table.Controls.Remove(table.GetControlFromPosition(0, s_index + 1));
+            /*
+             int i = 0;
+            foreach (KeyValuePair<string, dynamic> el in session_list[index].get_data())
+            {
+                Label tmp = new Label();
+                tmp.Text = el.Value.ToString();
+                tmp.AutoSize = true;
+                table.Controls.Add(tmp, i, s_index + 1);
+                i++;
+            }
+            Button remove = new Button();
+            remove.Text = "Удалить";
+            remove.Name = index.ToString();
+            remove.Click += new EventHandler(remove_item);
+            remove.AutoSize = true;
+            table.Controls.Add(remove, i, s_index + 1);
+            Button edit = new Button();
+            edit.Text = "Изменить";
+            edit.Name = index.ToString();
+            edit.Click += new EventHandler(edit_item);
+            edit.AutoSize = true;
+            table.Controls.Add(edit, i+1, s_index + 1);
+             */
+
+        }
+        private void create_new_button_Click(object sender, EventArgs e)
+        {
+            if (editable_index == -1)
+            {
+                try
+                {
+                    Tube new_tube = new Tube(int.Parse(number_field.Text), new Dimensions(double.Parse(size_X.Text),
+                        double.Parse(size_Y.Text), double.Parse(size_Z.Text)), double.Parse(weight_field.Text),
+                        (steel_type)Enum.Parse(typeof(steel_type), steel_selector.Text), is_good.Checked);
+                    if (actual_indexes.Count != 0)
+                        Add_tube(new_tube, actual_indexes.Max());
+                    else
+                        Add_tube(new_tube, 0);
+                    session_list.Add(new_tube);
+                    clear_fields();
+                    //Fill_table();
+                }
+                catch
+                {
+                    MessageBox.Show("Bad data!");
+                }
+            }
+            else
+            {
+                try
+                {
+                    Dimensions _new_dimensions = new Dimensions(double.Parse(size_X.Text), double.Parse(size_Y.Text), double.Parse(size_Z.Text));
+                    double _new_weight = double.Parse(weight_field.Text);
+                    steel_type _new_type = (steel_type)Enum.Parse(typeof(steel_type), steel_selector.Text);
+                    bool _new_state = is_good.Checked;
+                    session_list[editable_index].set_size(_new_dimensions);
+                    session_list[editable_index].set_type(_new_type);
+                    session_list[editable_index].set_weight(_new_weight);
+                    session_list[editable_index].set_state(_new_state);
+                    Update_tube(editable_index);
+                    clear_fields();
+                    //Fill_table();
+                }
+                catch
+                {
+                    MessageBox.Show("Bad data!");
+                }
+            }
+        }
+        private void remove_item(object sender, EventArgs e)
+        {
+            clear_fields();
+            Button current = sender as Button;
+            int f_index = actual_indexes.Where((i, index) => { return i == int.Parse(current.Name); }).First();
+            //System.Diagnostics.Debug.WriteLine(s_index);
+            int s_index = actual_indexes.IndexOf(f_index);
+            actual_indexes.Remove(f_index);
+            session_list.RemoveAt(s_index);
+            for (int i = 0; i < table.ColumnCount; i++)
+                table.Controls.Remove(table.GetControlFromPosition(0, s_index + 1));
+            //Fill_table();
+        }
+        private void edit_item(object sender, EventArgs e)
+        {
+            cancel_button.Enabled = true;
+            number_field.Enabled = false;
+            Button current = sender as Button;
+            Dictionary<string, dynamic> _current = session_list[int.Parse(current.Name)].get_data();
+            editable_index = int.Parse(current.Name);
+            Dimensions _size = (Dimensions)_current["size"];
+            number_field.Text = _current["id"].ToString();
+            weight_field.Text = _current["weight"].ToString();
+            size_X.Text = _size.size_x.ToString();
+            size_Y.Text = _size.size_y.ToString();
+            size_Z.Text = _size.size_y.ToString();
+            steel_selector.SelectedItem = _current["steel"].ToString();
+            is_good.Checked = _current["state"];
+        }
+        private void clear_fields()
+        {
+            editable_index = -1;
+            cancel_button.Enabled = false;
+            number_field.Enabled = true;
+            number_field.Clear();
+            weight_field.Clear();
+            size_X.Clear();
+            size_Y.Clear();
+            size_Z.Clear();
+            steel_selector.SelectedIndex = 0;
+            is_good.Checked = true;
+        }
+        private void cancel_button_Click(object sender, EventArgs e)
+        {
+            clear_fields();
+        }
+    }
+}
+
+enum steel_type
+{
+    steel1,
+    steel2,
+    steel3
+}
+class Dimensions
+{
+    public double size_x;
+    public double size_y;
+    public double size_z;
+    public Dimensions(double x, double y, double z)
+    {
+        this.size_x = x;
+        this.size_y = y;
+        this.size_z = z;
+    }
+    public override string ToString()
+    {
+        return String.Format("({0}, {1}, {2})", size_x, size_y, size_z);
+    }
+}
+class Tube
+{
+    private int number;
+    private bool state;
+    private steel_type type;
+    private Dimensions size;
+    private double weight;
+    /// <summary>
+    /// Труба с определенными свойствами
+    /// </summary>
+    /// <param name="number">№ трубы</param>
+    /// <param name="dimensions">размеры</param>
+    /// <param name="weight">вес</param>
+    /// <param name="type">марка стали</param>
+    /// <param name="state">качество (1 - годная, 0 - брак)</param>
+    public Tube(int number, Dimensions size, double weight, steel_type type, bool state = true)
+    {
+        this.number = number;
+        this.size = size;
+        this.weight = weight;
+        this.type = type;
+        this.state = state;
+    }
+    /// <summary>
+    /// Получить свойства трубы
+    /// </summary>
+    /// <returns>Словарь с ключами ["id", "state", "steel", "size", "weight"]</returns>
+    public Dictionary<string, dynamic> get_data()
+    {
+        Dictionary<string, dynamic> result = new Dictionary<string, dynamic>();
+        result["id"] = this.number;
+        result["state"] = this.state;
+        result["steel"] = this.type;
+        result["size"] = this.size;
+        result["weight"] = this.weight;
+        return result;
+    }
+    public void set_size(Dimensions size)
+    {
+        this.size = size;
+    }
+    public void set_weight(double weight)
+    {
+        this.weight = weight;
+    }
+    public void set_type(steel_type type)
+    {
+        this.type = type;
+    }
+    public void set_state(bool state)
+    {
+        this.state = state;
     }
 }
